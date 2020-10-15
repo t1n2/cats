292a4fb1b0aa2dd270edd7c2c2d333b7a99dc244
t1n2 <46750536+t1n2@users.noreply.github.com>
2020-10-15 14:53:02 +0300

    Изменен код html, css, JS
    Рефакторинг кода (добавлены функции), добавлен интервальный таймер для отзывов, изменен HTML и CSS (для отзывов).

--- js/script.js
+++ js/script.js
@@ -1,57 +1,95 @@
 'use strict';
 
-// Team Slider
+window.addEventListener('DOMContentLoaded', () => {
 
-const teamSlider = document.querySelectorAll('.item__border'),
-      teamContent = document.querySelectorAll('.team__item'),
-      sliderBtnNext = document.querySelectorAll('.team__item .slider__btn.slider__btn--next'),
-      sliderBtnPrev = document.querySelectorAll('.team__item .slider__btn.slider__btn--prev');
+	// Team Slider
 
-function hideSliderContent() {
-	teamSlider.forEach(item => {
-		item.classList.add('hide');
-		item.classList.remove('show');
+	const teamItem = document.querySelectorAll('.item__border'),
+	teamBtnNext = document.querySelectorAll('.team__item .slider__btn.slider__btn--next'),
+	teamBtnPrev = document.querySelectorAll('.team__item .slider__btn.slider__btn--prev');
+
+	function hideSliderContent(content) {
+		content.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show');
+		});
+	}
+
+	function showSliderContent(content, i) {
+		content[i].classList.remove('hide');
+		content[i].classList.add('show');
+	}
+
+	hideSliderContent(teamItem);
+	showSliderContent(teamItem, 0);
+
+	function sliderPrev(btn, content) {
+		btn.forEach((item, i) => {
+			item.addEventListener('click', () => {
+				if (i > 0 && i < btn.length) {
+					hideSliderContent(content);
+					showSliderContent(content, i - 1);
+				}
+			});
+		});
+	}
+
+	function sliderNext(btn, content) {
+		btn.forEach((item, i) => {
+			item.addEventListener('click', () => {
+				if (i < btn.length - 1) {
+					hideSliderContent(content);
+					showSliderContent(content, i + 1);
+				}
+			});
+		});
+	}
+
+	sliderPrev(teamBtnPrev, teamItem);
+	sliderNext(teamBtnNext, teamItem);
+
+
+	// FAQ accordeon
+
+	const faqBtn = document.querySelectorAll('.right__part'),
+	answer = document.querySelectorAll('.item__answer'),
+	itemSymbol = document.querySelectorAll('.item__symbol');
+
+	faqBtn.forEach((item, i) => {
+		item.addEventListener('click', () => {
+			item.classList.toggle('active');
+			answer[i].classList.toggle('show');
+			itemSymbol[i].classList.toggle('item__symbol--minus');
+		});
 	});
-}
 
-function showSliderContent(i) {
-	teamSlider[i].classList.remove('hide');
-	teamSlider[i].classList.add('show');
-}
+	// Reviews Slider
 
-hideSliderContent();
-showSliderContent(0);
+	const reviewsSlider = document.querySelectorAll('.reviews__item'),
+	reviewsBtnNext = document.querySelectorAll('.reviews__item .slider__btn.slider__btn--next'),
+	reviewsBtnPrev = document.querySelectorAll('.reviews__item .slider__btn.slider__btn--prev');
 
-sliderBtnPrev.forEach((item, i) => {
-	item.addEventListener('click', () => {
-	    if (i > 0 && i < sliderBtnNext.length) {
-			hideSliderContent();
-			showSliderContent(i - 1);
+	hideSliderContent(reviewsSlider);
+	showSliderContent(reviewsSlider, 0);
+
+	sliderPrev(reviewsBtnPrev, reviewsSlider);
+	sliderNext(reviewsBtnNext, reviewsSlider);
+	
+	let i = 0;
+	const timerId = setInterval(function () {
+		i++;
+        if (i < reviewsSlider.length) {
+			hideSliderContent(reviewsSlider);
+			showSliderContent(reviewsSlider, i);
+		} else {
+			i = 0;
+			hideSliderContent(reviewsSlider);
+			showSliderContent(reviewsSlider, i);
 		}
-	});
+	}, 3000);
+
 });
 
-sliderBtnNext.forEach((item, i) => {
-	item.addEventListener('click', () => {
-	    if (i < sliderBtnNext.length - 1) {
-			hideSliderContent();
-			showSliderContent(i + 1);
-		}
-	});
-});
 
-// FAQ accordeon
-
-const faqBtn = document.querySelectorAll('.right__part'),
-      answer = document.querySelectorAll('.item__answer'),
-      itemSymbol = document.querySelectorAll('.item__symbol');
-
-faqBtn.forEach((item, i) => {
-	item.addEventListener('click', () => {
-		item.classList.toggle('active');
-		answer[i].classList.toggle('show');
-		itemSymbol[i].classList.toggle('item__symbol--minus');
-	});
-});
 
 
